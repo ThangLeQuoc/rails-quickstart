dfb5dc28920ab28d2bac2c7bd7d09d1d0c5a6744
Thang Le Quoc <thanglequoc.it@gmail.com>
2020-03-07 20:13:17 +0700

    Add controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    puts 'Request to WelcomeController - index action'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # http://localhost:3000/welcome/index
+  get 'welcome/index'
+
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
